fc589aa9c5fc9daafde1f4408f46795e16e0d3cd
Martin Flores <martin.flores@bodybuilding.com>
2019-02-11 23:15:36 -0600

    agregando soporte inicial para amazon XRay

--- src/main/java/tec/mf/handler/ImageHandlerXRay.java
+++ src/main/java/tec/mf/handler/ImageHandlerXRay.java
@@ -1,18 +1,21 @@
 package tec.mf.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.entities.Subsegment;
 import tec.mf.handler.io.ImageHandlerResponseWriter;
 import tec.mf.handler.io.ImageRequest;
 import tec.mf.handler.io.InputEventParser;
 import tec.mf.handler.service.ImageService;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.function.Function;
 
-
-public class ImageHandler implements RequestStreamHandler {
+public class ImageHandlerXRay implements RequestStreamHandler {
 
     private static final AppConfig APP_CONFIG;
     private final AppConfig appConfig;
@@ -21,11 +24,11 @@ public class ImageHandler implements RequestStreamHandler {
         APP_CONFIG = AppConfig.getInstance();
     }
 
-    public ImageHandler() {
+    public ImageHandlerXRay() {
         this(APP_CONFIG);
     }
 
-    public ImageHandler(AppConfig appConfig) {
+    public ImageHandlerXRay(AppConfig appConfig) {
         this.appConfig = appConfig;
     }
 
@@ -33,7 +36,14 @@ public class ImageHandler implements RequestStreamHandler {
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
         LambdaLogger logger = context.getLogger();
         logger.log("Inside Image Handler ");
-        ImageRequest imageRequest = this.inputEventParser().processInputEvent(inputStream, logger);
+
+        ImageRequest imageRequest = AWSXRay.createSubsegment("input event", new Function<Subsegment, ImageRequest>() {
+            @Override
+            public ImageRequest apply(Subsegment subsegment) {
+                return inputEventParser().processInputEvent(inputStream, logger);
+            }
+        });
+
         InputStream imageResized = this.imageService().getImageFrom(imageRequest);
         this.imageHandlerResponseWriter().writeResponse(imageResized, outputStream, imageRequest);
     }
@@ -49,33 +59,4 @@ public class ImageHandler implements RequestStreamHandler {
     private ImageHandlerResponseWriter imageHandlerResponseWriter() {
         return this.appConfig.getImageHandlerResponseWriter();
     }
-
-//    private static String getStringFromInputStream(InputStream is) {
-//
-//        BufferedReader br = null;
-//        StringBuilder sb = new StringBuilder();
-//
-//        String line;
-//        try {
-//
-//            br = new BufferedReader(new InputStreamReader(is));
-//            while ((line = br.readLine()) != null) {
-//                sb.append(line);
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (br != null) {
-//                try {
-//                    br.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//
-//        return sb.toString();
-//
-//    }
 }
