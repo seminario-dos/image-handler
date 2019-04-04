0565747fac473f33aad693082e87650d09c65a65
Martin Flores <martin.flores@bodybuilding.com>
2019-04-03 20:18:03 -0600

    sh scripts for testing lambda

--- test-lambda.sh
+++ test-lambda.sh
@@ -7,6 +7,7 @@ echo $NUMBER_OF_INVOCATIONS
 for (( i=1; i<=NUMBER_OF_INVOCATIONS; i++ ))
 do
     echo "I VALUE IS: $i"
-    aws lambda invoke --function-name ImageHandlerXRay --log-type Tail --payload file://src/test/resources/image-request-event.json test-output.log
+    aws lambda invoke --function-name ImageHandlerSimple --log-type Tail --payload file://src/test/resources/image-request-event.json test-output.log
+#    aws lambda invoke --function-name ImageHandlerXRay --log-type Tail --payload file://src/test/resources/image-request-event.json test-output.log
     sleep 1
 done
