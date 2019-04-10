cac614868ea68be82f515461977cfe597fc521bc
Martin Flores <martin.flores@bodybuilding.com>
2019-04-09 21:34:43 -0600

    results when image is 5mb

--- test-lambda.sh
+++ test-lambda.sh
@@ -7,7 +7,8 @@ echo $NUMBER_OF_INVOCATIONS
 for (( i=1; i<=NUMBER_OF_INVOCATIONS; i++ ))
 do
     echo "I VALUE IS: $i"
-    aws lambda invoke --function-name ImageHandlerSimple --log-type Tail --payload file://src/test/resources/image-request-event.json test-output.log
+#    aws lambda invoke --function-name ImageHandler --log-type Tail --payload file://src/test/resources/image-request-event.json test-output.log
 #    aws lambda invoke --function-name ImageHandlerXRay --log-type Tail --payload file://src/test/resources/image-request-event.json test-output.log
+    aws lambda invoke --function-name ImageHandlerXRay --log-type Tail --payload file://src/test/resources/image-request-5mb-event.json test-output.log
     sleep 1
 done
