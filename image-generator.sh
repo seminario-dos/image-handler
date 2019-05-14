5726c879ef91ee92f25694177cbfef3236f44ae6
Martin Flores <martin.flores@bodybuilding.com>
2019-05-13 21:08:07 -0600

    agregando rutina para obtener imagenes de 500-1Mb

--- image-generator.sh
+++ image-generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NUMBER_OF_IMAGES=50
-NUMBER_OF_IMAGE_VERSIONS=20
+NUMBER_OF_IMAGES=5
+NUMBER_OF_IMAGE_VERSIONS=2
 IMAGE_COUNTER=1
 
 # Para generar el tamano aleatorio
@@ -17,6 +17,9 @@ MAX_NUMBER=4700
 # FOLDER_NAME=cluster-a
 FOLDER_NAME=cluster-b
 LOCAL_FOLDER_NAME=img/$FOLDER_NAME
+
+MIN_SIZE=512000
+MAX_SIZE=1024000
 
 
 IMAGE_IDS=$(curl -s https://picsum.photos/v2/list?page=1\&limit=$NUMBER_OF_IMAGES | jq '.[] | .id' | sed -e 's/^"//' -e 's/"$//' | tr "\n" " ")
@@ -40,9 +43,24 @@ for t in ${IMAGE_IDS[@]} ; do
 
         wget -O $LOCAL_IMAGE_NAME $IMAGE_URL
 
-        aws s3api put-object --bucket images-repo.dev --key $S3_KEY_NAME --body $LOCAL_IMAGE_NAME
+        IMAGE_SIZE=$(stat -f%z $LOCAL_IMAGE_NAME)
 
-        echo "$RANDOM_IMAGE_SIZE - $S3_KEY_NAME - $LOCAL_IMAGE_NAME - $IMAGE_URL - $IMAGE_COUNTER"
+        echo "$RANDOM_IMAGE_SIZE - $S3_KEY_NAME - $LOCAL_IMAGE_NAME - Size: $IMAGE_SIZE - $IMAGE_URL - $IMAGE_COUNTER"
+
+        while [ $IMAGE_SIZE -lt $MIN_SIZE ] || [ $IMAGE_SIZE -gt $MAX_SIZE ]; do
+            echo -e "\t ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+            echo -e "\t Invalid! $LOCAL_IMAGE_NAME - $IMAGE_SIZE"
+
+            RANDOM_IMAGE_SIZE=$(jot -r 1 $MIN_NUMBER $MAX_NUMBER)
+            IMAGE_URL="https://picsum.photos/id/$t/$RANDOM_IMAGE_SIZE"
+
+            wget -O $LOCAL_IMAGE_NAME $IMAGE_URL
+            IMAGE_SIZE=$(stat -f%z $LOCAL_IMAGE_NAME)
+
+            echo -e "\t $RANDOM_IMAGE_SIZE - $S3_KEY_NAME - $LOCAL_IMAGE_NAME - Size: $IMAGE_SIZE - $IMAGE_URL - $IMAGE_COUNTER"
+        done
+
+        aws s3api put-object --bucket images-repo.dev --key $S3_KEY_NAME --body $LOCAL_IMAGE_NAME
 
         rm $LOCAL_IMAGE_NAME
 
